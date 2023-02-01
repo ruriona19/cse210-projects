dfcd42cfd4f5d8bcbaa588779c77afc7eabbf58f
ruriona19 <roberto.uriona19@gmail.com>
2023-02-01 19:12:38 -0400

    Adding Learning 3 activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fn1 = new Fraction();
+        Console.WriteLine($"{fn1.GetTop()}/{fn1.GetBottom()}");
+        Fraction fn2 = new Fraction(5);
+        Console.WriteLine($"{fn2.GetTop()}/{fn2.GetBottom()}");
+        Fraction fn3 = new Fraction(3,4); 
+        Console.WriteLine($"{fn3.GetTop()}/{fn3.GetBottom()}");
+        Fraction fn4 = new Fraction(1,3); 
+        Console.WriteLine($"{fn4.GetTop()}/{fn4.GetBottom()}");
+        Console.WriteLine($"{fn4.GetDecimalValue()}");
     }
 }
